826a7488438074ec914cea4cf03e4b7294f58f20
DiegoSousa <diego@diegosousa.com>
2013-10-19 23:50:01 -0300

    Refactoring local Url

--- scripts/readTextFromHtmlJPA.lua
+++ scripts/readTextFromHtmlJPA.lua
@@ -7,18 +7,18 @@ local URL = "https://dl.dropboxusercontent.com/u/9338682/app/github_com_DiegoSou
 
 function handler (e)
 
-	if (e.class == 'ncl' and e.type == 'presentation' and e.action=='start') then
-		print('Imagem iniciada')
-		readTextFromHtml(URL)
-	end
+        if (e.class == 'ncl' and e.type == 'presentation' and e.action=='start') then
+                print('Imagem iniciada')
+                readTextFromHtml(URL)
+        end
 
 end
 
 
 function readTextFromHtml(url)
-	print("Acessando:", url) io.flush()
+        print("Acessando:", url) io.flush()
 
-	http.request(url, display)
+        http.request(url, display)
 end
 
 
@@ -27,28 +27,28 @@ end
 --body: corpo da resposta (neste caso, texto)
 function display(header, body)
 
-	print("Valor lido:", body) io.flush()
+        print("Valor lido:", body) io.flush()
 
-	geraEventoDeAtriuicao(PROPRIEDADE_NOME, body)
+        geraEventoDeAtriuicao(PROPRIEDADE_NOME, body)
 
 end
 
 
 function geraEventoDeAtriuicao(nomePropriedade, valor)
 
-		evt = {
-			class    = 'ncl',
-			type     = 'attribution',
-			action   = 'start',
-			name	 = nomePropriedade,
-			value    = tonumber(valor),
-		}
+                evt = {
+                        class    = 'ncl',
+                        type     = 'attribution',
+                        action   = 'start',
+                        name         = nomePropriedade,
+                        value    = tonumber(valor),
+                }
 
-		event.post(evt) -- evento de inicio da atribuicao
-		evt.action = 'stop'
-		event.post(evt) -- evento de final da atribuicao
+                event.post(evt) -- evento de inicio da atribuicao
+                evt.action = 'stop'
+                event.post(evt) -- evento de final da atribuicao
 
-		print('Evento de atribuicao gerado:', evt.value) io.flush()
+                print('Evento de atribuicao gerado:', evt.value) io.flush()
 end
 
 
